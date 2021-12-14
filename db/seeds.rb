9ea4b307f4c3790c301774cc287958285715b074
plmaurel69 <88190008+plmaurel69@users.noreply.github.com>
2021-12-14 22:18:03 +0100

    finish mvc

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
 require 'faker'
 
 puts 'Creating 10 fake restaurants...'
-10.times do
+2.times do
   restaurant = Restaurant.new(
     name:    Faker::Company.name,
     address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
-    #category: rand("chinese", "italian", "japanese", "french", "belgian"),
-    phone_number: Faker::PhoneNumber.phone_number
+    category: "japanese",
+    phone_number: Faker::PhoneNumber.phone_number,
   )
   restaurant.save!
 end
