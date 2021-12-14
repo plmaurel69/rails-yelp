fa390b64713ba06ab5eaee1d39a3f1618613e181
plmaurel69 <88190008+plmaurel69@users.noreply.github.com>
2021-12-14 19:36:24 +0100

    Create 2 models"
    "

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'faker'
+
+puts 'Creating 10 fake restaurants...'
+10.times do
+  restaurant = Restaurant.new(
+    name:    Faker::Company.name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    #category: rand("chinese", "italian", "japanese", "french", "belgian"),
+    phone_number: Faker::PhoneNumber.phone_number
+  )
+  restaurant.save!
+end
+puts 'Finished!'
